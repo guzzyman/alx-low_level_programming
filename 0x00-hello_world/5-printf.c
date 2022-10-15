6cfee586d26f3434f65f3207ee0f8e4895f0b0c8
Olotu Oluwagbemiga <>
2022-10-14 18:45:24 -0700

    Betty Printf task fix

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * * main entry point for the program
+ * Return (0) if no errors, return non zero
+ */
+int main(void)
+{
+	printf ("With proper grammer, but the outxoe is a piece of art,\n");
+	return (0);
+}
