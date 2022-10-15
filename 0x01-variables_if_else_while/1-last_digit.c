25411b7fe379d364d2cf6fb3476e4efa28023f30
Olotu Oluwagbemiga <>
2022-10-15 15:51:10 -0700

    Last number of a given random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int lastDigit;
-
-	lastDigit = n % 10;
+	int lastDigit = n % 10;
 
 	if (lastDigit > 5)
 		printf("%d and is greater than 5\n", lastDigit);
