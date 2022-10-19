bcd1c2ec4b504c9ce56abada3032a91421fd9aaa
guzzyman <gbenga.isess@gmail.com>
2022-10-19 06:20:15 +0100

    ALX functions and nested loop task
    ALX functions and nested loop task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - prints _putchar
+  *
+  *Return: returns 0
+  */
+
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+		_putchar(text[i]);
+	_putchar('\n');
+	return (0);
+}
