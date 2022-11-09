2128210a632e1abcf26559b1c1fa0886931ed866
guzzyman <gbenga.isess@gmail.com>
2022-11-09 05:28:56 +0100

    Malloc, memory allocation

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+void free_grid(int **grid, int height);
+char **strtow(char *str);
